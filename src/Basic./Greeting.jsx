1cc9e5a1ab8d9dd47d567329b779d687a615a87a
vaibhav <vaibhavsharma9526@gmail.com>
2022-04-13 21:22:02 +0530

    background image for all time period added

--- src/Basic./Greeting.jsx
+++ src/Basic./Greeting.jsx
@@ -8,24 +8,33 @@ const Greeting = () => {
     let greeting = "";
     // inline css
     const css = { };
+    //another css for background change
+    const Bgcss = { };
 
     // if , else if , else condition
     if (mytime >= 5 && mytime < 12) {
         greeting = "Good Morning";
         css.color = "orange";
+        Bgcss.backgroundImage = "url('https://wallpapercave.com/wp/V4F9sCQ.jpg')";
     } else if (mytime >=12 && mytime < 17){
         greeting = "Good Afternoon"
         css.color = "red";
+        Bgcss.backgroundImage = "url('https://cdn.pixabay.com/photo/2016/02/10/15/49/sky-1191949_1280.jpg')"
     } else if (mytime >= 17 && mytime < 20) {
-        greeting = "Good Evening"
+        greeting = "Good Evening";
+        css.color = "green";
+        Bgcss.backgroundImage = "url('https://wallpaperaccess.com/full/3695752.jpg')"
     } else {
         greeting = "Good Night !";
         css.color = "grey";
+        Bgcss.backgroundImage = "url('https://images.hdqwalls.com/download/anime-night-scenery-8r-1920x1080.jpg')"
     }
     return (
         <>
+        <div style={Bgcss} className="Main">
         <div className="Greeting">
         <h1>Hello Sir, <span style={css}>{greeting}</span></h1>
+        </div>
         </div>
         </>
     )
